8d140fad65c42ce6db6280579c95e1ec2dd7bebb
Johnny Kantaros <jkantaros@salesforce.com>
2025-10-13 17:11:04 -0700

    personal finance scaffolding

---
+++ src/plaid_client.py
@@ -0,0 +1,26 @@
+import os
+from plaid import ApiClient, Configuration, Environment
+from plaid.api import plaid_api
+
+PLAID_CLIENT_ID = os.getenv("PLAID_CLIENT_ID")
+PLAID_SECRET = os.getenv("PLAID_SECRET")
+PLAID_ENV = os.getenv("PLAID_ENV", "sandbox")
+
+# Set Plaid host
+host = {
+    "sandbox": Environment.Sandbox,
+    "production": Environment.Production,
+}.get(PLAID_ENV, Environment.Sandbox)
+
+# Configure Plaid client
+configuration = Configuration(
+    host=host,
+    api_key={
+        "clientId": PLAID_CLIENT_ID,
+        "secret": PLAID_SECRET,
+        "plaidVersion": "2020-09-14",
+    },
+)
+
+api_client = ApiClient(configuration)
+plaid_client = plaid_api.PlaidApi(api_client)
