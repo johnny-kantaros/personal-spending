c0aaf0c42a15da3eeeee5c1cd205521b1c7c196c
Johnny Kantaros <jkantaros@salesforce.com>
2026-04-17 21:09:38 -0700

    fix deployment - remove vercel.json

--- backend/src/db/db.py
+++ backend/src/db/db.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import sessionmaker
 from src.db.models import Base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./plaid.db")
+DATABASE_PATH = os.getenv("DATABASE_PATH", "./plaid.db")
+DATABASE_URL = f"sqlite:///{DATABASE_PATH}"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
